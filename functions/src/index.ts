004b4e4372158c3107e5779ca1ff35aad096e4c2
kokorau <kokorau@gmail.com>
2019-02-27 12:59:59 +0900

    create func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,17 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin'
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
 const app = admin.initializeApp()
 const db = app.firestore()
 
 export const onAccountCreate = functions.auth.user().onCreate(user => {
- db.collection('users').doc(user.uid).set({
-  displayName: user.displayName
- })
+    db.collection('users').doc(user.uid).set({
+        displayName: user.displayName
+    })
+})
+
+export const onEventCreate = functions.firestore.document('events/{eventId}').onCreate((snapshot, context) => {
+    snapshot.ref.collection('staff').add({
+        role: 'owner'
+    })
 })
